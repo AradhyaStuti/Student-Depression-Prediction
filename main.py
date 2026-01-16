fd5a0aa2001e42f2db5ddba2e1aac828e3759fc6
Aradhya Stuti <aradhya.mutants@gmail.com>
2026-01-16 09:52:41 +0530

    Comment out model training call in main.py
    made basic changes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.GUI import initialize
 from src.model_definition import train_and_export_model
 
-# Entrenamos el modelo (si no esta entrenado)
-# train_and_export_model()
 
-# Desplegamos la GUI
+# train_and_export_model()
 initialize()
